411f18998adf795158692ce4214b950ccf57b8c0
Mohanish <mohnishkumar7777777@gmail.com>
2024-09-11 19:46:31 +0530

    Delete bash.sh

--- bash.sh
+++
@@ -1,8 +0,0 @@
-# Navigate to the directory containing your Dockerfile
-docker build -t yourusername/alpine-terminal:latest .
-
-# Log in to Docker Hub
-docker login
-
-# Push the image to Docker Hub
-docker push yourusername/alpine-terminal:latest
